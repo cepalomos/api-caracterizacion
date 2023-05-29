c39a6a650b3aa0028e1bea1af4970f5de34bee5a
Carlos Eduardo Palomo Serna <cepalomos@outlook.es>
2023-05-29 12:30:45 -0500

    terminado base de datos

---
+++ src/models/Nucleo.js
@@ -0,0 +1,187 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("nucleo", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    zona: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["urbana", "rural"]],
+      },
+    },
+    corregimiento: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vereda: {
+      type: DataTypes.STRING,
+      defaultValue: "ninguna",
+    },
+    direccion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vivienda: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["propia", "arrendada", "familiar"]],
+      },
+    },
+    area: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "menos de 100 m²",
+            "entre 100 m² y 500 m²",
+            "entre 500 m² y 1000 m²",
+            "1/4 de ha",
+            "1/2 ha",
+            "1 ha",
+            "1.5 ha",
+            "2 ha",
+            "entre 3 y 5 has",
+            "entre 5 has y 10 has",
+            "entre 11 y 15 has",
+            "entre 16 has y 20 has",
+            "entre 21 has y 25 has",
+            "entre 26 has y 30 has",
+            "40 has",
+            "más de 50 has",
+          ],
+        ],
+      },
+    },
+    techo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["zinc", "palma", "eternit", "plastico"]],
+      },
+    },
+    paredes: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["bloque", "adobe", "bahareque", "plastico", "madera", "zinc"]],
+      },
+    },
+    piso: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["cemento", "plantilla", "baldosa", "tierra"]],
+      },
+    },
+    numHabitaciones: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["1", "2", "3", "4", "5", "Mas de 5"]],
+      },
+    },
+    numBanos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["1", "2", "3", "mas de cuatro"]],
+      },
+    },
+    conBanos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["Alcantarillado", "pozo septico", "ninguna de las anteriores"]],
+      },
+    },
+    preAlimentos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "espacio dentro de la casa para este fin",
+            "espacio fuera de la casa para este fin",
+            "en una área improvisada",
+          ],
+        ],
+      },
+    },
+    servicios: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+      validate: {
+        isValidOption: function (value) {
+          const allowedOptions = [
+            "energia",
+            "acueducto",
+            "alcantarillado",
+            "recoleccion de residuos",
+            "gas domiciliario",
+          ];
+          for (const option of value) {
+            if (!allowedOptions.includes(option)) {
+              throw new Error(`Opción no válida: ${option}`);
+            }
+          }
+        },
+      },
+    },
+    eneCocina: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "electricidad",
+            "petroleo",
+            "gas residencial",
+            "cilindro o pipeta",
+            "leña",
+          ],
+        ],
+      },
+    },
+    agua: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "acueducto por tuberia",
+            "pozo con bomba",
+            "pozo artesanal",
+            "agua lluvia de algibe",
+            "represa o jaguey",
+            "rio",
+            "arroyo",
+          ],
+        ],
+      },
+    },
+    residuos: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [
+          [
+            "recoleccion publica o privada",
+            "la entierran",
+            "la queman",
+            "disponen de lugar donde la echan",
+            "hacen clasificación de residuos",
+          ],
+        ],
+      },
+    },
+  });
+};
--- src/models/User.js
+++ src/models/User.js
@@ -24,14 +24,9 @@ module.exports = (sequelize) => {
         type: DataTypes.INTEGER,
         allowNull: false,
         validate: {
-            min: 0,
-            max: 130,
+          min: 0,
+          max: 130,
         },
-      },
-
-      direccion: {
-        type: DataTypes.STRING,
-        allowNull: false,
       },
 
       parentesco: {
@@ -46,22 +41,107 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          len: [0, 20],
+          isIn: [["masculino", "femenino", "lgtbi", "otro"]],
         },
       },
-
+      isEstudio: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      estudio: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [
+            [
+              "primaria",
+              "iletrado",
+              "bachillerato",
+              "tecnico",
+              "tecnico culminado",
+              "tecnico inconcluso",
+              "universitario",
+              "universitario inconcluso",
+              "universitario culminado",
+            ],
+          ],
+        },
+      },
+      isUniversidad: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      etnia: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [
+            [
+              "indigena",
+              "afrodesendiente",
+              "raizal",
+              "palenquero",
+              "Rom",
+              "mestizo",
+            ],
+          ],
+        },
+      },
       actividad: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          isIn: [
+            [
+              "Campesino agrícola",
+              "Campesino pecuario",
+              "Campesino mixto",
+              "Campesino piscícola",
+              "Jornal",
+              "Campesino y jornal",
+              "Empleado público",
+              "Madre comunitaria",
+              "Ama de casa",
+              "Comerciante",
+              "Moto taxi",
+              "Empleada doméstica",
+              "Conductor",
+              "Tendero",
+              "Desempleo",
+              "Trabaja como independiente",
+              "Trabaja con el sector privado",
+              "Otros",
+            ],
+          ],
+        },
       },
 
       salario: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.DECIMAL(10, 2),
         allowNull: false,
       },
-    },
-    {
-      timestamps: false,
+      salud: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [["contributivo", "subsidiado"]],
+        },
+      },
+      ispension: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      isDiscapacitado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      isVictima: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     }
+    // {
+    //   timestamps: false,
+    // }
   );
 };
