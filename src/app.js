71b72639d19837453547b749ba99f305e4a42938
Carlos Eduardo Palomo Serna <cepalomos@outlook.es>
2023-06-22 07:24:11 -0500

    Agregacion de cors

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const { responseError } = require("./response/response");
 
 const server = express();
 
 server.use(express.json());
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(cors({
+  origin: "http://localhost:3000", // Reemplaza con la URL del frontend
+  credentials: true
+}));
 server.use("/", routes);
 
 server.use(responseError);
